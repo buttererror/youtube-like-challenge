178654457fe610f49f9db0fda38827722bff9926
Mahmoud Ahmed <buttererror@gmail.com>
2020-01-30 02:36:10 +0200

    add css components, add youtube logo

---
+++ src/components/input-search.vue
@@ -0,0 +1,19 @@
+<template>
+     <div></div>
+     <!--<input type="text" @keyup.enter="search">-->
+</template>
+
+<script>
+   export default {
+      name: "input-search",
+      methods: {
+         search() {
+
+         }
+      }
+   }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/layout.vue
@@ -0,0 +1,20 @@
+<template>
+     <main class="main">
+          <header class="heading">
+               <yotube-logo></yotube-logo>
+               <input-search></input-search>
+          </header>
+     </main>
+</template>
+
+<script>
+   import YotubeLogo from "./yotube-logo";
+   import InputSearch from "./input-search";
+   export default {
+      name: "layout",
+      components: {InputSearch, YotubeLogo}
+   }
+</script>
+
+<style lang="scss">
+</style>
